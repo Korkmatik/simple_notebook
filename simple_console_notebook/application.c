3a2be98685976b44977f9376026e7c80b9ff3e8e
Korkmatik <fahri.korkmaz@protonmail.ch>
2019-02-05 19:40:19 +0100

    Added functionality to delete notes

--- simple_console_notebook/application.c
+++ simple_console_notebook/application.c
@@ -19,7 +19,7 @@ static void quit_show_all_notebooks(char* error_string, FILE** fstream,
 		char* buffer);
 static void open_notebook();
 static void show_notes_menu();
-static void print_all_notes();
+static int print_all_notes();
 static bool is_notebook_opened();
 static void create_new_entry();
 static void delete_note();
@@ -338,14 +338,14 @@ static void show_notes_menu() {
 	flush_stdin();
 }
 
-static void print_all_notes() {
+static int print_all_notes() {
 	if (!is_notebook_opened()) {
 			current_notebook.fstream = fopen(current_notebook.to_text, "rb");
 			if (current_notebook.fstream == NULL) {
 				quit_submenu_with_error(
 						"No notebook opened yet. Please open a notebook first.",
 						NULL, NULL, NULL);
-				return;
+				return -1;
 			}
 		} else {
 			current_notebook.fstream = freopen(current_notebook.to_text, "rb",
@@ -367,6 +367,8 @@ static void print_all_notes() {
 			printf("Sorry, no notes stored so far\n");
 
 		free(buffer);
+
+		return line;
 }
 
 static bool is_notebook_opened() {
@@ -428,13 +430,79 @@ static void create_new_entry() {
 }
 
 static void delete_note() {
-	printf("Do you want to show all notes first?(y/n)");
-	char user_choice = getchar();
 	printf("\n");
-	if (user_choice == 'y' || user_choice == 'Y')
-		show_notes_menu();
+	int number_notes = print_all_notes();
+	if (number_notes <= 0)
+		return;
 
+	flush_stdin();
+	printf("\nWhich note do you want to delete? Enter the number of the note: ");
+	char user_choice = getchar();
+	int delete_line = user_choice - '0' - 1; // Transforming the char into an integer
 
+	if (delete_line < 0 || delete_line > number_notes) {
+		quit_submenu_with_error("No such note", NULL, NULL, NULL);
+		return;
+	}
+
+	/* Deleting the note*/
+	//Allocating memory for the name of the temporary file
+	char* temp_filename = malloc(strlen(current_notebook.to_text) + 5);
+	if (temp_filename == NULL) {
+		quit_submenu_with_error("Error: Could not allocate memory for temp_buffer", NULL, NULL, NULL);
+		return;
+	}
+	strcpy(temp_filename, current_notebook.to_text);
+	strcat(temp_filename, ".tmp");
+
+	FILE* temp_file = fopen(temp_filename, "wb");
+	if (temp_file == NULL) {
+		quit_submenu_with_error("Error: Could not create temp_file", temp_filename, NULL, &temp_file);
+		return;
+	}
+
+	current_notebook.fstream = freopen(current_notebook.to_text, "rb", current_notebook.fstream);
+
+	if (current_notebook.fstream == NULL) {
+		quit_submenu_with_error("Error: Could not reopen current_notebook.fstream!", temp_filename, NULL, &temp_file);
+		return;
+	}
+
+	// Allocate memory for the buffer which will hold the notes
+	int current_line = 0;
+	size_t buffer_size = 2048;
+	char* line_buffer = malloc(buffer_size);
+	if (line_buffer == NULL) {
+		quit_submenu_with_error("Error: Could not allocate memory for line_buffer", temp_filename, NULL, &temp_file);
+		return;
+	}
+
+	// Write all notes which won't be deleted into the temporary file
+	while(getline(&line_buffer, &buffer_size, current_notebook.fstream) != -1) {
+		if (current_line != delete_line)
+			fprintf(temp_file, "%s", line_buffer);
+		current_line++;
+	}
+
+	// Rename temporary file into current notebook name
+	fclose(current_notebook.fstream);
+	fclose(temp_file);
+	remove(current_notebook.to_text);
+	rename(temp_filename, current_notebook.to_text);
+
+	free(line_buffer);
+	free(temp_filename);
+
+	current_notebook.fstream = fopen(current_notebook.to_text, "rb");
+	if (current_notebook.fstream == NULL) {
+		quit_submenu_with_error("Error while trying to reopen current notebook", NULL, NULL, NULL);
+		strcpy(current_notebook.to_text, "");
+		return;
+	}
+
+	printf("SUCCESS: Note was deleted!\nPress [ENTER] to return to menu.");
+	flush_stdin();
+	getchar();
 }
 
 _Noreturn static void quit() {
