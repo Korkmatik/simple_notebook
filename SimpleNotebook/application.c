ccb4e92eae3b172a7f24027d5cb0289db3ea6d20
Korkmatik <fahri.korkmaz@protonmail.ch>
2019-02-09 12:24:18 +0100

    Added functionality to load last opened notebook on application startup

--- SimpleNotebook/application.c
+++ SimpleNotebook/application.c
@@ -14,18 +14,35 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define LAST_NOTEBOOK_SAVE "last_notebook"
+
+static void load_notebook(CURRENT_NOTEBOOK *current_notebook);
 static void handle_user_input(CURRENT_NOTEBOOK *current_notebook);
 static void quit(CURRENT_NOTEBOOK *current_notebook);
 
 
 void start_application() {
 	CURRENT_NOTEBOOK current_notebook;
-	current_notebook.fstream = NULL;
-	strcpy(current_notebook.to_text, "");
-
+	load_notebook(&current_notebook);
+	
 	while (1) {
 		show_menu(&current_notebook);
 		handle_user_input(&current_notebook);
+	}
+}
+
+void load_notebook(CURRENT_NOTEBOOK *current_notebook) {
+	FILE* last_notebook = fopen(LAST_NOTEBOOK_SAVE, "rb");
+	if (last_notebook == NULL)
+		return;
+	
+	fread(current_notebook, sizeof(CURRENT_NOTEBOOK), 1, last_notebook);
+	fclose(last_notebook);
+
+	current_notebook->fstream = fopen(current_notebook->to_text, "rb");
+	if (current_notebook->fstream == NULL) {
+		strcpy(current_notebook->to_text, "");
+		return;
 	}
 }
 
@@ -66,6 +83,14 @@ void handle_user_input(CURRENT_NOTEBOOK *current_notebook) {
 
 static void quit(CURRENT_NOTEBOOK* current_notebook) {
 	printf("GOODBYE!\n");
+
+	if (current_notebook->fstream != NULL) {
+		FILE* last_notebook = fopen(LAST_NOTEBOOK_SAVE, "wb");
+		if (last_notebook != NULL) {
+			fwrite(current_notebook, sizeof(CURRENT_NOTEBOOK), 1, last_notebook);
+			fclose(last_notebook);
+		}		
+	}
 
 	// Closing if there is an opened notebook
 	if (current_notebook->fstream != NULL)
